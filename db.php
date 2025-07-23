25096327d4dc79c0a9b36fa4771c6af4ca57d2f9
gandromes <moto.pers2007@gmail.com>
2025-07-23 13:54:53 +0400

    composer vlucas/phpdotenv is Deprecated

--- db.php
+++ db.php
@@ -1,13 +1,10 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
 $host = '127.0.0.1';
 $dbname = 'smartsoft_test';
-$username = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASS'];
+$username = 'root';
+$password = '!12A34bAb!';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password, [
